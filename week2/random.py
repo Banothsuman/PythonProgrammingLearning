f7f3c0069577bb6289820345bed7ce37b3751eab
Banothsuman <banothsuman84036@gmail.com>
2025-03-27 11:09:31 +0530

    Create random.py

---
+++ week2/random.py
@@ -0,0 +1,11 @@
+mport random
+n=random.randint(1,100)
+while True:
+    x=int(input("enter the value of x"))
+    print("Guess the number")
+    if(x<n):
+      print("too Low")
+    elif(x>n):
+      print("too high")
+    else:
+      print("Game over")
